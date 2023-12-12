84c5fa8e62a70f07265a0fd8dd2c7f3d3d00bc7d
B2N10X30 <samuelbonux10@gmail.com>
2023-12-12 19:06:40 +0100

    wrking

--- database/queries.go
+++ database/queries.go
@@ -18,24 +18,6 @@ func CreateUserTable() error {
 	return nil
 }
 
-// actually tables won't be created everytime so won't be used
-func CreateTaskTable() error {
-	query := `CREATE TABLE IF NOT EXISTS tasks (
-		task_id SERIAL PRIMARY KEY,
-		user_id INT REFERENCES users(user_id),
-		description VARCHAR(255) NOT NULL,
-		status VARCHAR(20) NOT NULL,
-		start_time TIMESTAMP NOT NULL,
-		due_date TIMESTAMP NOT NULL,
-		completion_time TIMESTAMP,
-	);`
-	_, err := dB.Exec(query)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 // Creates User and Update the database
 func CreateUser(username, password string) error {
 	query := `INSERT INTO users (username, password_hash)
@@ -47,7 +29,7 @@ func CreateUser(username, password string) error {
 	return nil
 }
 
-// some kind of login mechanism
+// some kinda login mechanism
 func SelectDetails(username string) (string, error) {
 	query := `SELECT password_hash FROM users where username = $1`
 	var password string
@@ -58,8 +40,28 @@ func SelectDetails(username string) (string, error) {
 	return password, nil
 }
 
+// actually tables won't be created everytime so won't be used
+func CreateTaskTable() error {
+	query := `CREATE TABLE IF NOT EXISTS tasks (
+        task_id SERIAL PRIMARY KEY,
+        user_id INT,
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        description VARCHAR(255) NOT NULL,
+        status VARCHAR(20) NOT NULL,
+        start_time TIMESTAMP NOT NULL,
+        due_date TIMESTAMP NOT NULL,
+        completion_time TIMESTAMP
+    );`
+	_, err := dB.Exec(query)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// insert task with user_id
 func InsertTask(description, status string, startAt, dueAt time.Time) error {
-	query := `INSERT INTO tasks (description, status, start_time, due_date, completion_time)
+	query := `INSERT INTO tasks (user_id, description, status, start_time, due_date, completion_time)
 	VALUES ($1, $2,$3, $4, $5)`
 	_, err := dB.Exec(query, description, status, startAt, dueAt)
 	if err != nil {
